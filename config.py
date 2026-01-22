006a6fec2c0facfa8a833bc09f92ecef9f27a3f5
william20230323-stack <william20230323@gmail.com>
2026-01-22 08:00:25 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-# 對接 GitHub 上的 TG_TOKEN
-RADAR_TOKEN = os.getenv("TG_TOKEN")
-# 對接 GitHub 上的 TG_CHAT_ID
-RADAR_CHAT_ID = os.getenv("TG_CHAT_ID")
-# 對接標的名稱
-SYMBOL = os.getenv("TRADE_SYMBOL", "DUSKUSDT")
